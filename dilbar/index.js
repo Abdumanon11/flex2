d6d1ce28b7286954237d77aa596cd62f5e29d0fe
Abdumanon11 <143066287+Abdumanon11@users.noreply.github.com>
2023-11-09 18:27:57 +0500

    Add files via upload

---
+++ dilbar/index.js
@@ -0,0 +1,30 @@
+/* let Name = prompt("Виберите  муку , масла  поравшийся  вам Dilbar_50$ , Kazakistan_60$,Elena Маленький_5$, Elena Большой_8$, E'zoz_40$ , Kazakistan 2 сорт_70$")
+if (Name  == "Dilbar_50$ , Kazakistan_60$,Elena Маленький_5$, Elena Большой_8$, E'zoz_40$ , Kazakistan 2 сорт_70$") {
+    alert ("перейти к оплате " + Name)
+    let num = prompt("Оплатите")
+    if (num ==="7777") {
+    
+
+}else {
+    alert ("go home")
+}
+        alert ("welcome " + num )
+        let m = prompt("How much money do you want?")
+
+    if (age >= 1000 && age <= 10000){
+
+alert ("welcome" + m )
+
+
+
+    }else{
+      alert ("not enough money")
+    
+    }
+    
+        
+        
+        alert ("go home")
+    }else {
+    }
+     */
